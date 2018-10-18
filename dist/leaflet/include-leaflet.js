f6d29291b95461324a002f0a512d51a9b3be63fe
gooin <gooin@outlook.com>
2018-10-18 10:27:09 +0800

    update

--- dist/leaflet/include-leaflet.js
+++ dist/leaflet/include-leaflet.js
@@ -78,9 +78,9 @@
         }
         if (!inArray(excludes, 'iclient9-leaflet')) {
             if (supportES6() && !inArray(includes, 'iclient9-plot-leaflet')) {
-                inputScript("./dist/leaflet/iclient9-leaflet-es6.js");
+                inputScript("/dist/leaflet/iclient9-leaflet-es6.js");
             } else {
-                inputScript("./dist/leaflet/iclient9-leaflet.js");
+                inputScript("/dist/leaflet/iclient9-leaflet.js");
             }
         }
         if (inArray(includes, 'iclient9-leaflet-css')) {
